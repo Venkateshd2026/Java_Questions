fbebf641f56bf944597b799451be89a8686a046e
Venkatesh D <venkateshd2026@gmail.com>
2026-07-16 18:31:18 +0530

    Time: 0 ms (100%), Space: 41.9 MB (72.76%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1){
+            return 1;
+        }
+        int f=1;
+        int s=2;
+        for(int i=3;i<=n;i++){
+            int c=f+s;
+            f=s;
+            s=c;
+        }
+        return s;
+        
+    }
+}
