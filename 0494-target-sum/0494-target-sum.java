d48d66a21c3775b6835eebe169ad8d29ecdbd4cd
Venkatesh D <venkateshd2026@gmail.com>
2026-07-21 14:32:47 +0530

    Time: 5 ms (80.33%), Space: 45.4 MB (33.18%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int n=nums.length;
+      int sum=0;
+      for(int i=0;i<n;i++){
+        sum=sum+nums[i];
+      }
+      if(Math.abs(target)>sum){
+        return 0;
+      }
+      if((sum+target)%2!=0){
+        return 0;
+      }
+      int k=(sum+target)/2;
+      int dp[][]=new int[n+1][k+1];
+      for(int i=0;i<=n;i++){
+        dp[i][0]=1;
+      }
+      for(int i=1;i<=n;i++){
+        for(int j=0;j<=k;j++){
+            if(nums[i-1]<=j){
+                dp[i][j]=dp[i-1][j]+dp[i-1][j-nums[i-1]];
+            }else{
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+      }
+      return dp[n][k];
+    }
+}
