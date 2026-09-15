02448f74b239af0d22e2cf412471656236d2d988
Venkatesh D <venkateshd2026@gmail.com>
2026-09-15 23:46:28 +0530

    Time: 5 ms (63.82%), Space: 57.4 MB (69.12%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -1,15 +1,10 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
 var isPalindrome = function(s) {
     s = s.toLowerCase().replace(/[^a-z0-9]/g, "");
 
-    let left = 0;
-    let right = s.length - 1;
-
-    while (left < right) {
-        if (s[left] !== s[right]) {
-            return false;
-        }
-        left++;
-        right--;
-    }
-    return true;
+    let res=s.split("").reverse().join("");
+    return (res===s);
 };
